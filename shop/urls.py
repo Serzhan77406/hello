042b2643415bbc59c6bffc670cee17f7b36f1d32
Serzhan77406 <aigul.dauletyar@mail.ru>
2026-01-26 21:40:01 +0500

    получилось

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    if request.user.is_authehticated:
+        return HttpResponse(f'привет {request.user.username}')
+    return HttpResponse('Вы вoшли')
 
 
 urlpatterns = [
