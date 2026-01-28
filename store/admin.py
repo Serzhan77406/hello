f73028d384b39d27ba6bcd9d4f18c01b67fab15e
Serzhan77406 <Aigul.dauletyar@mail.ru>
2026-01-28 22:01:25 +0500

    добавили cart

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from .cart import Cart, CartItem
 
 
 admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+
+
+
+
+
